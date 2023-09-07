a820bcb5e425168a19288cea41728445dfe9dd97
KaiLangen96 <>
2023-09-07 17:23:46 +0000

    Add new arrivals to index.html

--- home/views.py
+++ home/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from products.models import Product
 
 
 def index(request):
     """ A view to return the index page """
-
-    return render(request, 'home/index.html')
+    products = Product.objects.filter(category__name="new_arrivals")
+    context = {
+        'products': products,
+    }
+    print(products)
+    return render(request, 'home/index.html', context)
