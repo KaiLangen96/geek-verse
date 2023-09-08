abdb5c12ef7f065cbbabd805760b24da26c33456
KaiLangen96 <>
2023-09-08 11:47:39 +0000

    Adjust new arrivals section in index.html

--- home/views.py
+++ home/views.py
@@ -4,9 +4,8 @@ from products.models import Product
 
 def index(request):
     """ A view to return the index page """
-    products = Product.objects.filter(category__name="new_arrivals")
+    products = Product.objects.filter(category__name="new_arrivals")[:6]
     context = {
         'products': products,
     }
-    print(products)
     return render(request, 'home/index.html', context)
