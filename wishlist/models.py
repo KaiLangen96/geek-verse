2ddba75e27c495e2377cf0cadc68ae258c19550e
KaiLangen96 <>
2023-09-14 10:03:15 +0000

    Add url and template to wishlist

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
-# Create your models here.
+
+class WishlistItem(models.Model):
+    """
+    A wishlist item model to give users the ability to create
+    a wishlist of items in the store.
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f""
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render, reverse, get_object_or_404)
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from products.models import Product
+from wishlist.models import WishlistItem
+
+
+@login_required
+def view_wishlist(request):
+    """ Displays the user's wishlist """
+    wishlist = WishlistItem.objects.filter(user=request.user)
+    template = 'wishlist/wishlist.html'
+    context = {
+        'wishlist': wishlist,
+        'on_profile_page': True,
+    }
+
+    return render(request, template, context)
