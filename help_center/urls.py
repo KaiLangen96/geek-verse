b6378512724775bda59a306740be053de9a8d11a
KaiLangen96 <>
2023-09-26 10:41:44 +0000

    Add submit question feature

--- help_center/urls.py
+++ help_center/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.view_help_center, name="view_help_center"),
+    path("submit/", views.submit_question, name="submit_question"),
 ]
--- help_center/views.py
+++ help_center/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 from .forms import QuestionForm
@@ -15,4 +16,33 @@ def view_help_center(request):
         "question_form": question_form,
     }
 
+    return render(request, template, context)
+
+
+@login_required
+def submit_question(request):
+    """ Posts the questions to the admin via the contact form """
+    if request.method == 'POST':
+        question_form = QuestionForm(request.POST, initial={"user": request.user})
+        question_form.fields['user'].disabled = True
+
+        if question_form.is_valid():
+            question = question_form.save()
+            template = "help_center/question_submitted.html"
+            context = {
+                "user": request.user,
+                "question": question,
+            }
+            return render(request, template, context)
+    else:
+        question_form = QuestionForm(initial={"user": request.user})
+        question_form.fields['user'].disabled = True
+
+    messages.error(
+        request, "The question form is invalid. Please ensure the form is valid."
+    )
+    template = "help_center/help_center.html"
+    context = {
+        "question_form": question_form,
+    }
     return render(request, template, context)
